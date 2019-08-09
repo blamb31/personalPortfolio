d4c9cee5206827b3284eeb632389bbbbaa02bbca
Blake Lamb <blake.lamb31@gmail.com>
2019-08-09 00:10:15 -0600

    Finished styling the mobile version of the home page. Still need the profile picture

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react' 
 
+import gitHubLogo from '../../githubwhite.png'
+import linkedInLogo from '../../LinkedIn_logo_initials.png'
+import footerStyles from './footer.module.css'
+
 function Footer(props) {
     return (
-        <div>
-            Footer
+        <div className={footerStyles.logoContainer}>
+            <img className={footerStyles.logos} src={gitHubLogo}/>
+            <img className={footerStyles.logos} src={linkedInLogo}/>
+
         </div>
     )
 }
